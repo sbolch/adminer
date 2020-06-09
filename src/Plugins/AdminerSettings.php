d4fb0ccad9b7d53880a679405f18601929308353
Szabolcs Surányi <subbysnake@gmail.com>
2020-06-09 10:48:52 +0200

    Route and notice fixes

--- src/Plugins/AdminerSettings.php
+++ src/Plugins/AdminerSettings.php
@@ -5,6 +5,6 @@ namespace Plugins;
 class AdminerSettings {
 
     function navigation() {
-        echo '<a href="/settings.php" style="font-size: 1.25em; position: absolute; top: .5em; right: .5em;" title="Settings">&#128736;</a>';
+        echo '<a href="./settings.php" style="font-size: 1.25em; position: absolute; top: .5em; right: .5em;" title="Settings">&#128736;</a>';
     }
 }
